4d82c5976b460bb3049a67c382e958b733685973
Isaac Denny <isaacquinndenny@gmail.com>
2023-03-03 22:03:35 -0700

    redux now persists state across renders

--- wed-reg-client/src/app/store.js
+++ wed-reg-client/src/app/store.js
@@ -1,8 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
 import authReducer from "../features/auth/authSlice";
 
-export const store = configureStore({
-  reducer: {
-    authReducer: authReducer
-  },
-});
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+let store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+let persistor = persistStore(store);
+
+export default {store, persistor};
--- wed-reg-client/src/index.js
+++ wed-reg-client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./app/store"
+import store from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
         <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
