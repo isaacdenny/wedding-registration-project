353fb115cd0ffc444dc93f74e562b3fa8e4475bd
Isaac Denny <isaacquinndenny@gmail.com>
2023-03-31 13:35:56 -0600

    Event Details page

--- client/src/components/navigation/Footer.js
+++ client/src/components/navigation/Footer.js
@@ -5,35 +5,16 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <ul className="nav-links">
-        <li>
-          <Link className="nav-link" to="/our-story">
-            Our Story
-          </Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/about-bridal">
-            About Bridal
-          </Link>
-        </li>
-      </ul>
       <Link to="/" style={{ textDecoration: "none" }}>
         <h1 className="nav-logo">
           Allie <HiHeart /> Isaac
         </h1>
       </Link>
-      <ul className="nav-links">
-        <li>
-          <Link className="nav-link" to="/hotel-rec">
-            Hotel Rec
-          </Link>
+      <div className="nav-links">
+        <li className="email">
+          Please Email us at: allieandisaacwedding@gmail.com
         </li>
-        <li>
-          <Link className="nav-link" to="/event-details">
-            Event Details
-          </Link>
-        </li>
-      </ul>
+      </div>
     </div>
   );
 };
--- client/src/pages/EventDetailsPage.js
+++ client/src/pages/EventDetailsPage.js
@@ -3,9 +3,9 @@ import Footer from "../components/navigation/Footer";
 import Navbar from "../components/navigation/Navbar";
 import BlockSection from "../components/sections/BlockSection";
 import HeaderSection from "../components/sections/HeaderSection";
-import image1 from "../images/engagement-hotel.jpg";
+import image1 from "../images/kingdomchurch2.jpg";
 import image2 from "../images/engagement-ring.jpg";
-import image3 from "../images/rings.jpg";
+import image3 from "../images/gift-card.jpg";
 
 const EventDetailsPage = () => {
   return (
@@ -15,7 +15,7 @@ const EventDetailsPage = () => {
       <BlockSection
         isAlone={true}
         heading="Location"
-        desc="event desc goes here..."
+        desc="The Wedding will be held at Kingdom Church on 910 Shoshone St N, Twin Falls, ID 83301"
         imgSrc={image1}
         imgAlt="engagement ring with pink rose"
         isFlipped={true}
@@ -23,18 +23,18 @@ const EventDetailsPage = () => {
       />
       <BlockSection
         isAlone={true}
-        heading="Event Detail 2"
-        desc="event desc goes here..."
-        imgSrc={image2}
+        heading="Gifts!"
+        desc="If you would like to bring a gift, please consider giving GIFTCARDS rather than larger items, as we will be moving soon and not be able to bring everything with us :( Thank you so much!"
+        imgSrc={image3}
         imgAlt="engagement hotel with flowers"
         isFlipped={false}
         button={false}
       />
       <BlockSection
         isAlone={true}
-        heading="Event Detail 3"
-        desc="event desc goes here..."
-        imgSrc={image3}
+        heading="When Does It Start?"
+        desc="Our Wedding will begin promptly at 4:00pm and go until 8:30-9:00pm! The ceremony will be short, and there will be a reception immediately after!"
+        imgSrc={image2}
         imgAlt="engagement hotel with flowers"
         isFlipped={true}
         button={false}
